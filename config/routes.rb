c381c916734eb834ccea00c4257916a1678c4924
Peter Poo Yi Sing <pooyisingwork@gmail.com>
2018-01-20 15:47:50 +0800

    landingpage done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :controllers
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root to: redirect('/users/sign_in')
