3799aa03c94d0d2a4004306e6ee1ba0fc58e6b8a
Peter Poo Yi Sing <pooyisingwork@gmail.com>
2018-01-27 16:46:44 +0800

    doing following function. there's a bug.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,15 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, except: [:signout]
   # Rmb to exlude the "signout" method from "authenticate_user!" method or else the sign out page will not be rendered after user signs out, because the user will have to login again in order to see the sign out page.
 
-  # before_action :prepare_user, only: []
-  before_action :prepare_user, only: [:tweets, :following, :follower]
+  before_action :prepare_user, only: [:tweets, :followings, :followers]
   before_action :prepare_current_user, only: [:home, :tweets_by_tag]
 
-
+  # This is the current_user HOME page. It will show the tweets of the all the users that current_user follow as well as his own tweet.
   def home
     @tweets = @user.feed
-    tag = Tag
   end
 
+  # This is the page of all other USER. It will show the tweets of the particular user that current_user clicked on only.
   def tweets
     @tweets = @user.tweets.order(created_at: :desc) # order OWN tweets according to when they were created, with the most recent tweet at the top.
     respond_to do |format|
@@ -21,10 +20,12 @@ class PagesController < ApplicationController
     end
   end
 
+  # This is the page of the particular user that current_user clicked on for that user's "Following" in that user's profile card. It will show all the people which that particular user followed.
   def followings
     @followees = @user.followees.order(created_at: :desc) # order the followings(the people that current_user follows) according to when they were followed by current_user, with the most recent following at the top.
   end
 
+  # This is the page of the particular user that current_user clicked on for that user's "Followers" in that user's profile card. It will show all the people that followed that particular user.
   def followers
     @followers = @user.followers.order(created_at: :desc) # order the followers(the people that follows current_user) according to when they follow current_user, with the most recent follower at the top.
   end
@@ -35,6 +36,7 @@ class PagesController < ApplicationController
     @tweets = @tag.tweets.order(created_at: :desc)  # order the tweets according to when they were created, with the most recent tweet at the top.
   end
 
+  # ==================================== To be continued ==============================================================
   def search
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
 
   before_action :prepare_user
+  before_action :prepare_followee, only: [:follow, :unfollow]
 
   def update_avatar
     @user.update(avatar: avatar)
     flash[:notice] = 'Avatar updated!'
-    redirect_to authenticated_root_path
+    redirect_back fallback_location: authenticated_root_path
   end
+
+  def follow
+    @user.follow(@followee)   # @followee : the person whom current_user is tracking.
+    # Followee:   a person who is BEING TRACKED on a social media website or application.
+    flash[:notice] = "You followed " + @followee.username + " !"
+    redirect_back fallback_location: authenticated_root_path
+  end
+
+  def unfollow
+    @user.follow(@followee) # @followee : the person whom current_user is tracking.
+    # Followee:   a person who is BEING TRACKED on a social media website or application.
+    flash[:notice] = "You unfollowed " + @followee.username + " !"
+    redirect_back fallback_location: authenticated_root_path
+  end
+
 
   private
 
@@ -14,12 +30,33 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def prepare_followee
+    @followee = User.find(params[:id])
+  end
+
   def avatar
     params.require(:avatar)
   end
 
-  def book_params
-    params.require(:book).permit(:name, :image, :remove_image)
-  end
-
 end
+
+# ================================================== Notes ===================================================================
+=begin
+
+redirect_back method
+
+  redirect_back(fallback_location:, **args) Link
+
+  Redirects the browser to the page that issued the request (the referrer) if possible, otherwise redirects to the provided default fallback location.
+
+  The referrer information is pulled from the HTTP `Referer` (sic) header on the request. This is an optional header and its presence on the request is subject to browser security settings and user preferences. If the request is missing this header, the fallback_location will be used.
+
+  redirect_back fallback_location: { action: "show", id: 5 }
+  redirect_back fallback_location: @post
+  redirect_back fallback_location: "http://www.rubyonrails.org"
+  redirect_back fallback_location: "/images/screenshot.jpg"
+  redirect_back fallback_location: posts_url
+  redirect_back fallback_location: proc { edit_post_url(@post) }
+  All options that can be passed to redirect_to are accepted as options and the behavior is identical.
+
+=end
